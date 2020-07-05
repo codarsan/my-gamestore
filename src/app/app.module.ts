412b45d1fb03ac923701b349591868e912af2d18
Houssem BEN SLIMANE <h.benslimane@sofrecom-tunisie.com>
2020-07-05 17:23:23 +0100

    updated to angular 10, removed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -35,7 +34,6 @@ import { FeedbackListComponent } from './components/feedback-list/feedback-list.
     FeedbackListComponent
   ],
   imports: [
-    BsDropdownModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
